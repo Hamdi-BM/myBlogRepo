5edfb58532df283c2abbf7b0b05f07ac8ecc4dc7
hamdi <hamdibm331@gmail.com>
2024-07-03 18:37:08 +0100

    feat: creating the routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,25 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import React from 'react'
+import Home from '../pages/Home'
+import SignIn from '../pages/Signin'
+import About from '../pages/about'
+import SignUp from '../pages/SignUp'
+import Projects from '../pages/Projects'
+import Dashboard from '../pages/Dashboard'
 
 export default function App() {
   return (
-    <h1 className='text-3xl'>
-      app
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/SignIn" element={<SignIn/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/SignUp" element={<SignUp/>}/>
+        <Route path="/Projects" element={<Projects/>}/>
+        <Route path="/Dashboard" element={<Dashboard/>}/>
+
+        
+      </Routes>
+    </BrowserRouter>
   )
 }
